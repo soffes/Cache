e031f2773013944d9e1bab7426c29212cc2431f2
Sam Soffes <sam@soff.es>
2016-05-16 14:04:11 -0700

    Carthage support

---
+++ Support/Cache.h
@@ -0,0 +1,15 @@
+//
+//  Cache.h
+//  Cache
+//
+//  Created by Sam Soffes on 5/16/16.
+//  Copyright © 2016 Sam Soffes. All rights reserved.
+//
+
+@import Foundation;
+
+//! Project version number for Cache.
+FOUNDATION_EXPORT double CacheVersionNumber;
+
+//! Project version string for Cache.
+FOUNDATION_EXPORT const unsigned char CacheVersionString[];
